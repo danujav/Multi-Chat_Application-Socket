743b4e3cca6b949c7090153a8b74fefdcf81d08a
DanujaV <danujagreru@gmail.com>
2021-01-09 10:08:27 +0530

    create Server Side Server class and ServerThread class

---
+++ Command_Line Multi-Chat Program/src/Server_Side/Server.java
@@ -0,0 +1,43 @@
+package Server_Side;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Server {
+
+    private static Socket socket;
+    private static ServerSocket serverSocket;
+
+    public static void main(String[] args) {
+
+        //list to add all the client Thread
+        ArrayList <ServerThread> threadArrayList = new ArrayList<>();
+
+        try(ServerSocket serverSocket = new ServerSocket(5000)){
+
+            System.out.println("Server is waiting for the clients requests..!");
+
+            while(true){
+                Socket socket = serverSocket.accept();
+                ServerThread serverThread = new ServerThread(socket, threadArrayList);
+
+                //Starting the Thread
+                threadArrayList.add(serverThread);
+                serverThread.start();
+
+                //get all the list of currently running thread
+            }
+        } catch (IOException e) {
+            System.out.println("Error occurred in main: " + e.getStackTrace());
+        }finally{
+            try {
+                socket.close();
+                serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ Command_Line Multi-Chat Program/src/Server_Side/ServerThread.java
@@ -0,0 +1,53 @@
+package Server_Side;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ServerThread extends Thread{
+    private Socket socket;
+    private ArrayList<ServerThread> threadArrayList;
+    private PrintWriter output;
+
+    public ServerThread(){}
+
+    public ServerThread(Socket socket, ArrayList<ServerThread> threads){
+        this.socket = socket;
+        this.threadArrayList = threads;
+    }
+
+    @Override
+    public void run(){
+        try{
+            //Reading the input from Client
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            /*returning the output to the client : true statement is to flush the buffer otherwise
+            we have to do it manually*/
+            output = new PrintWriter(socket.getOutputStream(), true);
+
+            //infinite loop for server
+            while(true){
+                String outputString = input.readLine();
+
+                //if user type exit command then program will terminate
+                if(outputString.equals("exit")){
+                    break;
+                }
+                printToAllClients(outputString);
+                System.out.println("Server received " + outputString);
+            }
+        } catch (Exception e){
+            System.out.println("Error occurred " + e.getStackTrace());
+        }
+    }
+
+    private void printToAllClients(String outputString) {
+        for (ServerThread sT : threadArrayList) {
+            sT.output.println(outputString);
+        }
+    }
+}
