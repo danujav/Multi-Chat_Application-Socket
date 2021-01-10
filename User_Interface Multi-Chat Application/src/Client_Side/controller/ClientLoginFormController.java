0146195167b56743fb2e99902241eeae0a6a746d
DanujaV <danujagreru@gmail.com>
2021-01-10 14:30:10 +0530

    create client side nulti chatting but need to improve. have lot of bugs

--- User_Interface Multi-Chat Application/src/Client_Side/controller/ClientLoginFormController.java
+++ User_Interface Multi-Chat Application/src/Client_Side/controller/ClientLoginFormController.java
@@ -1,8 +1,56 @@
 package Client_Side.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class ClientLoginFormController {
-    public void continueBtnOnAction(ActionEvent actionEvent) {
+    public TextField txtName;
+    public String name;
+    public AnchorPane mainRoot;
+    public static String userName;
+
+    public static ArrayList<String> users = new ArrayList<>();
+
+
+    public void continueBtnOnAction(ActionEvent actionEvent) throws IOException {
+       /* if(txtName.getText() != null){
+         //   lblContactName.setText(txtName.getText());
+            System.out.println(txtName.getText());
+            this.mainRoot.getChildren().clear();
+            this.mainRoot.getChildren().add(FXMLLoader.load(this.getClass().getResource("../view/MessegeEnvironmentForm.fxml")));
+        }*/
+        userName = txtName.getText().trim();
+        boolean flag = false;
+        if(users.isEmpty()){
+            users.add(userName);
+            flag = true;
+        }
+
+        for(String s : users){
+            if (!s.equalsIgnoreCase(userName)) {
+                flag = true;
+                System.out.println(userName);
+                break;
+            }
+        }
+
+        if(flag){
+         /*     if(txtName.getText() != null){
+         //   lblContactName.setText(txtName.getText());
+            System.out.println(txtName.getText());
+                  this.mainRoot.getChildren().clear();
+                  this.mainRoot.getChildren().add(FXMLLoader.load(this.getClass().getResource("../view/MessegeEnvironmentForm.fxml")));*/
+
+            this.mainRoot.getChildren().clear();
+            this.mainRoot.getChildren().add(FXMLLoader.load(this.getClass().getResource("../view/MessegeEnvironmentForm.fxml")));
+        }
+
     }
 }
--- User_Interface Multi-Chat Application/src/Client_Side/controller/MessageEnvironmentFormController.java
+++ User_Interface Multi-Chat Application/src/Client_Side/controller/MessageEnvironmentFormController.java
@@ -1,4 +1,79 @@
 package Client_Side.controller;
 
-public class MessageEnvironmentFormController {
+import javafx.event.ActionEvent;
+import javafx.geometry.NodeOrientation;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class MessageEnvironmentFormController extends Thread{
+    public Label lblContactName;
+    public TextArea txtArea;
+    public TextField txtMessage;
+
+    public BufferedReader reader;
+    public PrintWriter writer;
+    public Socket socket;
+
+    public void initialize(){
+        try{
+            socket = new Socket("localhost", 5000);
+            System.out.println("Socket is connecting with server");
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            writer = new PrintWriter(socket.getOutputStream(), true);
+            this.start();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void txtMsgOnAction(ActionEvent actionEvent) {
+        String msg = txtMessage.getText().trim();
+        writer.println(ClientLoginFormController.userName + ": "+ msg);
+        txtArea.setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
+        txtArea.appendText("Me: " + msg + "\n\n");
+        txtMessage.setText("");
+        if(msg.equalsIgnoreCase("Bye") || (msg.equalsIgnoreCase("logout"))){
+            System.exit(0);
+        }
+    }
+
+    @Override
+    public void run() {
+        try{
+            while (true){
+                String msg = reader.readLine();
+                String[] tokens = msg.split(" ");
+                String cmd = tokens[0];
+                StringBuilder fullMessage = new StringBuilder();
+                for (int i = 1; i < tokens.length; i++) {
+                    fullMessage.append(tokens[i]);
+                }
+
+                System.out.println(fullMessage);
+
+                if(cmd.equalsIgnoreCase(ClientLoginFormController.userName + ": ")){
+                    continue;
+                }else if (fullMessage.toString().equalsIgnoreCase("bye")){
+                    break;
+                }
+
+                txtArea.appendText(msg + "\n\n");
+            }
+
+            reader.close();
+            writer.close();
+            socket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
